6630a857d164199ab725bd00af8e2aef50d10786
Mathew Chebet <kiproye@gmail.com>
2025-11-09 14:04:24 -0500

    Adding a migration to create the DB

---
+++ GeographyAPI/Migrations/20251109190314_Initial_Migration.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeographyAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial_Migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Language",
+                columns: table => new
+                {
+                    LanguageID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LevelOfDifficuly = table.Column<int>(type: "int", nullable: false),
+                    NumberOfSpeakers = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Language", x => x.LanguageID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Country",
+                columns: table => new
+                {
+                    CountryID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Capital = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Population = table.Column<int>(type: "int", nullable: false),
+                    Independence = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TriviaFact = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LanguageID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Country", x => x.CountryID);
+                    table.ForeignKey(
+                        name: "FK_Country_Language_LanguageID",
+                        column: x => x.LanguageID,
+                        principalTable: "Language",
+                        principalColumn: "LanguageID");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CountryLanguage",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CountryID = table.Column<int>(type: "int", nullable: false),
+                    LanguageID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CountryLanguage", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CountryLanguage_Country_CountryID",
+                        column: x => x.CountryID,
+                        principalTable: "Country",
+                        principalColumn: "CountryID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CountryLanguage_Language_LanguageID",
+                        column: x => x.LanguageID,
+                        principalTable: "Language",
+                        principalColumn: "LanguageID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Country_LanguageID",
+                table: "Country",
+                column: "LanguageID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CountryLanguage_CountryID",
+                table: "CountryLanguage",
+                column: "CountryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CountryLanguage_LanguageID",
+                table: "CountryLanguage",
+                column: "LanguageID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CountryLanguage");
+
+            migrationBuilder.DropTable(
+                name: "Country");
+
+            migrationBuilder.DropTable(
+                name: "Language");
+        }
+    }
+}
