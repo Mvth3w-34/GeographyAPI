ec5c63fb24408226f98f2a2c09c7e393c68b9edb
Mathew Chebet <kiproye@gmail.com>
2025-11-09 10:29:49 -0500

    Adding a DbContext class

---
+++ GeographyAPI/Data/GeographyContext.cs
@@ -0,0 +1,19 @@
+﻿using GeographyAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeographyAPI.Data
+{
+    public class GeographyContext : DbContext
+    {
+        public GeographyContext(DbContextOptions<GeographyContext> opt) : base()
+        {
+
+        }
+
+        public DbSet<Language> Language { get; set; }
+        public DbSet<Country> Country { get; set; }
+
+        public DbSet<CountryLanguage> CountryLanguage { get; set; }
+
+    }
+}
