01abb654669f61dc4b8dd1d40e72e6fc750a2070
Mathew Chebet <kiproye@gmail.com>
2025-11-11 10:42:47 -0500

    Adding a new migration to reflect the changes made to the models

--- GeographyAPI/Migrations/20251111151902_FixingColumns&AddingValidation.Designer.cs
+++ GeographyAPI/Migrations/20251111151902_FixingColumns&AddingValidation.Designer.cs
@@ -3,6 +3,7 @@ using GeographyAPI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GeographyAPI.Migrations
 {
     [DbContext(typeof(GeographyContext))]
-    partial class GeographyContextModelSnapshot : ModelSnapshot
+    [Migration("20251111151902_FixingColumns&AddingValidation")]
+    partial class FixingColumnsAddingValidation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GeographyAPI/Migrations/20251111151902_FixingColumns&AddingValidation.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeographyAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixingColumnsAddingValidation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LevelOfDifficuly",
+                table: "Language");
+
+            migrationBuilder.DropColumn(
+                name: "Independence",
+                table: "Country");
+
+            migrationBuilder.DropColumn(
+                name: "Population",
+                table: "Country");
+
+            migrationBuilder.RenameColumn(
+                name: "NumberOfSpeakers",
+                table: "Language",
+                newName: "HoursOfStudyForProficiency");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Language",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FSIRank",
+                table: "Language",
+                type: "nvarchar(3)",
+                maxLength: 3,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TriviaFact",
+                table: "Country",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Country",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Capital",
+                table: "Country",
+                type: "nvarchar(16)",
+                maxLength: 16,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FSIRank",
+                table: "Language");
+
+            migrationBuilder.RenameColumn(
+                name: "HoursOfStudyForProficiency",
+                table: "Language",
+                newName: "NumberOfSpeakers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Language",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LevelOfDifficuly",
+                table: "Language",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "TriviaFact",
+                table: "Country",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(300)",
+                oldMaxLength: 300,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Country",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Capital",
+                table: "Country",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(16)",
+                oldMaxLength: 16);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Independence",
+                table: "Country",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Population",
+                table: "Country",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
