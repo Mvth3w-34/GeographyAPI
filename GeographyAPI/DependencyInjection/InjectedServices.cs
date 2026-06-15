7e80b6dbc2e2f7d79b9bd70da8045995fde33f3f
Mathew Chebet <kiproye@gmail.com>
2026-06-15 15:49:20 -0400

    Adding services required for jwt

--- GeographyAPI/DependencyInjection/InjectedServices.cs
+++ GeographyAPI/DependencyInjection/InjectedServices.cs
@@ -1,7 +1,12 @@
+using GeographyAPI.Authentication;
 using GeographyAPI.Data;
 using GeographyAPI.Repositories;
+using GeographyAPI.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using System.Threading.RateLimiting;
 
 public static class InjectedServices
@@ -15,6 +20,8 @@ public static class InjectedServices
             options.UseSqlServer(connectionString ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.")));
         services.AddScoped<ICountryRepo, CountryRepo>();
         services.AddScoped<ILanguageRepo, LanguageRepo>();
+        services.AddScoped<IJwtGenerator, JwtGenerator>();
+        services.AddScoped<IJwtManagerService, JwtManagerService>();
 
         //Adding Rate Limiting
         services.AddRateLimiter(options =>
@@ -29,6 +36,35 @@ public static class InjectedServices
                 options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
                 options.QueueLimit = 0;
             });
+        });
+
+        // JWT bearer authentication
+
+        var jwtSecret = configuration["Jwt:Secret"];
+        var key = Encoding.UTF8.GetBytes(jwtSecret);
+        var securityKey = new SymmetricSecurityKey(key);
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        }).AddJwtBearer(jwtOptions =>
+        {
+            jwtOptions.RequireHttpsMetadata = false;
+            jwtOptions.TokenValidationParameters = new TokenValidationParameters
+            {
+
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateIssuerSigningKey = true,
+
+                IssuerSigningKey = securityKey,
+                ValidAudience = configuration["Jwt:Audience"],
+                ValidIssuer = configuration["Jwt:Issuer"],
+
+                ClockSkew = TimeSpan.Zero
+            };
+
         });
 
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
