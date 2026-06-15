680e046e0d77f53089c3add2d096eefe26fcb449
Mathew Chebet <kiproye@gmail.com>
2026-06-15 15:52:00 -0400

    Adding an authentication controller and an endpoint to serve jwts

---
+++ GeographyAPI/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using GeographyAPI.DTOs.Request;
+using GeographyAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GeographyAPI.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        private readonly IJwtManagerService _jwtManagerService;
+
+        public AuthController(IJwtManagerService jwtManagerService)
+        {
+            _jwtManagerService = jwtManagerService;
+        }
+
+        [HttpPost]
+        public ActionResult<string> CreateToken(TokenRequestDTO token)
+        {
+            if (ModelState.IsValid)
+            {
+
+                string jwtToken = _jwtManagerService.GenerateToken(token);
+
+                return jwtToken;
+            }
+
+            return BadRequest();
+        }
+
+    }
+}
