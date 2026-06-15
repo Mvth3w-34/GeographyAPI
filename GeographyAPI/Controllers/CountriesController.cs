6c503a57e01219499eba05a7d5515cfb059b51a9
Mathew Chebet <kiproye@gmail.com>
2026-06-15 15:52:31 -0400

    Adding authourization for the countries controller

--- GeographyAPI/Controllers/CountriesController.cs
+++ GeographyAPI/Controllers/CountriesController.cs
@@ -1,5 +1,6 @@
 ﻿using GeographyAPI.DTOs.External;
 using GeographyAPI.Repositories;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -8,6 +9,7 @@ namespace GeographyAPI.Controllers
 {
     [Route("api/countries")]
     [ApiController]
+    [Authorize]
     public class CountriesController : ControllerBase
     {
         private readonly ICountryRepo _countryRepo;
