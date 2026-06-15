5850f58058ebd63b5e067013af438208dd45224c
Mathew Chebet <kiproye@gmail.com>
2026-06-15 11:04:58 -0400

    Creating a class and an interface to generate jwts

---
+++ GeographyAPI/Repositories/IJwtGenerator.cs
@@ -0,0 +1,9 @@
+﻿using GeographyAPI.DTOs.Request;
+
+namespace GeographyAPI.Repositories
+{
+    public interface IJwtGenerator
+    {
+        string CreateToken(TokenRequestDTO request);
+    }
+}
