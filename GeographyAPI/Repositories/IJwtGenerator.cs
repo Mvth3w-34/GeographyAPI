6c834c9b533f704dadaae50eaf6cee32c31525c9
Mathew Chebet <kiproye@gmail.com>
2026-06-15 12:48:51 -0400

    Renaming folder structure

--- GeographyAPI/Repositories/IJwtGenerator.cs
+++
@@ -1,9 +0,0 @@
-﻿using GeographyAPI.DTOs.Request;
-
-namespace GeographyAPI.Repositories
-{
-    public interface IJwtGenerator
-    {
-        string CreateToken(TokenRequestDTO request);
-    }
-}
