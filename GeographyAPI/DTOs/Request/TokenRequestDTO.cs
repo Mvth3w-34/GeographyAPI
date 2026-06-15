4aeaffcb5a575380dd0a1e027e81b4c956660d15
Mathew Chebet <kiproye@gmail.com>
2026-06-15 15:50:24 -0400

    Adding validation to the request DTO

---
+++ GeographyAPI/DTOs/Request/TokenRequestDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GeographyAPI.DTOs.Request
+{
+    public record TokenRequestDTO
+    {
+        [Required]
+        public string ClientId { get; set; }
+
+        [Required]
+        public string Secret { get; set; }
+    }
+}
